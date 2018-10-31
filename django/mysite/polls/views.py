d051348abc049fe386682a42d032f3e4ad1100a8
DESKTOP-V9EF5JF\joe <96joeichiro@gmail.com>
2018-10-31 23:39:58 +0900

    fix to avoid race conditions by using F

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
+from django.db.models import F
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
@@ -33,7 +34,7 @@ def vote(request, question_id):
             'error_message': "You didn't select a choice.",
         })
     else:
-        selected_choice.votes += 1
+        selected_choice.votes = F('votes') + 1
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
