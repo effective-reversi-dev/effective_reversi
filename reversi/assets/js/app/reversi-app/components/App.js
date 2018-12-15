aeb3103f603ecf7eac3e04e8f7e00e41fa362567
Joichiro <96joeichiro@gmail.com>
2018-12-16 00:23:10 +0900

    Add AppBar with some items (menu icon, title, and exit icon)

--- reversi/assets/js/app/reversi-app/components/App.js
+++ reversi/assets/js/app/reversi-app/components/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import MyLayout from './game-components/MyLayout'
+import MyLayout from './game-components/MyLayout';
+
+import AppBar from '@material-ui/core/AppBar';
+import ExitToApp from '@material-ui/icons/ExitToApp';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 class App extends React.Component {
     render() {
         return(
             <div className='App'>
+                <AppBar position='static' color='default'>
+                    <Toolbar>
+                        <Grid container justify='space-between' spacing={24}>
+                            <IconButton>
+                                <MenuIcon />
+                            </IconButton>
+                            <Typography variant='h6'>
+                               Effective-Reversi
+                            </Typography>
+                            <IconButton>
+                                <ExitToApp />
+                            </IconButton>
+                        </Grid>
+                    </Toolbar>
+                </AppBar>
                 <MyLayout/>
             </div>
         );
