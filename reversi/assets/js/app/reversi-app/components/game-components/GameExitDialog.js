3f9ae631c540b5904bae85630ccb195aa9a297f6
Keitaro Kitagawa <polestar1592@yahoo.co.jp>
2018-12-23 14:47:22 +0900

    Exitボタンのアクションを実装 (#10)

---
+++ reversi/assets/js/app/reversi-app/components/game-components/GameExitDialog.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import Button from '@material-ui/core/Button';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+class GameExitDialog extends React.Component {
+    render() {
+        return (
+           <Dialog
+            open={this.props.isOpenExitDialog}
+            onClose={() => this.props.handleExitDialog(flase)}
+            aria-labelledby="alert-leave-title"
+            aria-describedby="alert-leave-description"
+            >
+                <DialogTitle id="alert-leave-title">{"退出しますか?"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-leave-description">
+                        ゲームから退出しますか?退出したら負けになります。
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => this.props.handleExitDialog(false)} color="primary" autoFocus>
+                        キャンセル
+                    </Button>
+                    <Button onClick={() => this.props.handleExitDialog(false)}>
+                        退出する
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        )
+    }
+}
+
+export default GameExitDialog
