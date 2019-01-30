c05b76468d87a20037fae0d89e6aa9a41ef565b5
Keitaro Kitagawa <polestar1592@yahoo.co.jp>
2019-01-31 00:36:38 +0900

    add login page and redirect

--- reversi/reversi/urls.py
+++ reversi/reversi/urls.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.conf.urls import include, url  # noqa
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
 from exampleapp.view.signup import signup
@@ -12,7 +13,8 @@ urlpatterns = [
     url(r'^jsreverse/$', django_js_reverse.views.urls_js, name='js_reverse'),
     url(r'^signup/$', signup, name='signup'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
-    url(r'^$', TemplateView.as_view(template_name='exampleapp/itworks.html'), name='home'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
+    url(r'^$', login_required(TemplateView.as_view(template_name='exampleapp/itworks.html')), name='home'),
 ]
 
 if settings.DEBUG:
