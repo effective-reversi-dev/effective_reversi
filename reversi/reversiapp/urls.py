facf3733d6a466dab2651009dc50ec66adf15e68
Keitaro Kitagawa <polestar1592@yahoo.co.jp>
2019-03-09 23:19:45 +0900

    game画面, config画面へ外部サイトから直接アクセスしても適切にルーティングされるよう修正

--- reversi/reversiapp/urls.py
+++ reversi/reversiapp/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from reversiapp.view.reversiapp import reversiapp
 
 urlpatterns = [
-    path('', reversiapp, name='home')
+    path('', reversiapp, name='home'),
+    path('game', reversiapp, name='game'),
+    path('config', reversiapp, name='config')
 ]
