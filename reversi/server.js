16b40ad42f1a94cb660bdc267c4ecf8ce32a9996
Joichiro <96joeichiro@gmail.com>
2019-04-25 00:10:44 +0900

    server: change to open browser automatically when running webpack development server

--- reversi/server.js
+++ reversi/server.js
@@ -1,4 +1,5 @@
 // Webpack dev server
+import openBrowser from 'react-dev-utils/openBrowser';
 import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
 import config from './webpack.local.config';
@@ -9,10 +10,12 @@ new WebpackDevServer(webpack(config), {
   inline: true,
   historyApiFallback: true,
   headers: { 'Access-Control-Allow-Origin': '*' }
-}).listen(3000, '0.0.0.0', (err) => {
+}).listen(3000, '0.0.0.0', err => {
   if (err) {
-    console.log(err);
+    return console.log(err);
   }
-
   console.log('Listening at 0.0.0.0:3000');
+  if (openBrowser('http://127.0.0.1:8000')) {
+    console.log('Opening the browser tab...');
+  }
 });
